9884511a40e069a84faae4529000d5da84acbf7c
Preacher-1 <liuyutian20041121@gmail.com>
2024-12-02 21:18:35 +0800

    残差模块测试

--- NVAE_residual_module.py
+++ NVAE_residual_module.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 from torch.nn.modules.batchnorm import _BatchNorm
 from torchvision import models
 from torchvision.transforms import transforms
+from utils import get_parameter_number
 
 """
 网格表示+Transformer 编码器+解码器
@@ -50,19 +51,24 @@ class Cell(nn.Module):
         self.bn_swish1 = nn.BatchNorm2d(in_channels * expansion)
 
         self.conv2 = nn.Sequential(
-            nn.Conv2d(in_channels * expansion, in_channels * expansion, kernel_size=5, stride=stride, padding=2,
-                      groups=in_channels * expansion),
+            nn.Conv2d(in_channels * expansion, in_channels * expansion, kernel_size=3, stride=stride, padding=1,
+                      groups=in_channels * expansion, bias=True, dilation=1),
             nn.BatchNorm2d(in_channels * expansion),
-            nn.Conv2d(in_channels * expansion, in_channels * expansion, kernel_size=1, bias=False),
-            nn.BatchNorm2d(in_channels * expansion)
         )  # depthwise separable convolution
         self.bn_swish2 = nn.BatchNorm2d(in_channels * expansion)
         self.conv3 = nn.Conv2d(in_channels * expansion, out_channels, kernel_size=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channels)
         self.se = SE(out_channels, out_channels)
 
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+
     def forward(self, x):
-        skip = x
+        skip = self.shortcut(x)
         x = self.bn1(x)  # C
         x = self.conv1(x)  # EC
 
@@ -84,3 +90,27 @@ class Cell(nn.Module):
 class ConvTower(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(ConvTower, self).__init__()
+        self.cell1 = Cell(in_channels, 128, stride=2)
+        self.cell2 = Cell(128, 256, stride=1)
+        self.cell3 = Cell(256, 512, stride=1)
+        self.cell4 = Cell(512, 1024, stride=1)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(1024, 10)
+
+    def forward(self, x):
+        x = self.cell1(x)
+        x = self.cell2(x)
+        x = self.cell3(x)
+        x = self.cell4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+
+if __name__ == '__main__':
+    model = ConvTower(3, 10)
+    print(get_parameter_number(model))
+    x = torch.randn(1, 3, 32, 32)
+    y = model(x)
+    print(y.shape)
---
+++ utils.py
@@ -0,0 +1,4 @@
+def get_parameter_number(model):
+    total_num = sum(p.numel() for p in model.parameters())
+    trainable_num = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    return {'Total': total_num, 'Trainable': trainable_num}
